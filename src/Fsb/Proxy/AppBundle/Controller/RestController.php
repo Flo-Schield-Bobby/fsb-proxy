65e82995945a8a5865e76ea2d1d340706a1dcdeb
fschildknecht <florent.schildknecht@gmail.com>
2015-06-30 17:55:41 +0200

    Restore Guzzle proxy calls

--- src/Fsb/Proxy/AppBundle/Controller/RestController.php
+++ src/Fsb/Proxy/AppBundle/Controller/RestController.php
@@ -4,11 +4,9 @@ namespace Fsb\Proxy\AppBundle\Controller;
 
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\DomCrawler\Crawler;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 use GuzzleHttp\Psr7\Request as Psr7Request;
-
 
 class RestController extends Controller
 {
@@ -22,26 +20,13 @@ class RestController extends Controller
 			'query' => $request->query->all()
 		), $request->getContent());
 
-		$promise = $client->sendAsync($proxyRequest)->then(function ($proxyResponse) use ($client, $proxyRequest, $response) {
-			$response->setStatusCode($proxyResponse->getStatusCode());
-			$response->headers->replace($proxyResponse->getHeaders());
+		$proxyResponse = $client->send($proxyRequest);
 
-			$dom = new Crawler($proxyResponse->getBody()->getContents());
-			$baseUri = (String) $client->getConfig()['base_uri'];
+		$response->setStatusCode($proxyResponse->getStatusCode());
+		$response->headers->replace($proxyResponse->getHeaders());
 
-			$dom->filter('link')->each(function (Crawler $node, $i) use ($baseUri) {
-				$node->getNode(0)->setAttribute('href', preg_replace("#(.?)/(.+)#", $baseUri . '$2', $node->attr('href')));
-			});
+		$response->setContent($proxyResponse->getBody());
 
-			$dom->filter('script')->each(function (Crawler $node, $i) use ($baseUri) {
-				$node->getNode(0)->setAttribute('src', preg_replace("#(.?)/(.+)#", $baseUri . '$2', $node->attr('src')));
-			});
-
-			$response->setContent($dom->html());
-
-			$response->send();
-		});
-
-		$promise->wait();
+		return $response;
 	}
 }
